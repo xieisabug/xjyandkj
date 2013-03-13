6c982c9bf26897de361d71074e7e7a25c186b516
xjyaitf@gmail.com <xjyaitf@gmail.com@ee9c0801-8353-6db2-6eb2-82078e8949fa>
2013-03-13 16:36:24 +0000

    测试一些代码

---
+++ trunk/TeamHome/test/com/teamhome/dao/login/TestLoginDao.java
@@ -0,0 +1,43 @@
+package com.teamhome.dao.login;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestLoginDao {
+
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void testLogin(){
+		fail();
+	}
+	
+	@Test
+	public void testLogout(){
+		fail();
+	}
+	
+	@Test
+	public void testSQLLogin(){
+		fail();
+	}
+	
+	@Test
+	public void testNoUsernameLogin(){
+		fail();
+	}
+	
+	@Test
+	public void testNoPasswordLogin(){
+		fail();
+	}
+	
+	@Test
+	public void testNotLogin(){
+		fail();
+	}
+}
