c2060205fbe800989135dfe6c48d0c07910c8f30
xjyaitf@gmail.com <xjyaitf@gmail.com@ee9c0801-8353-6db2-6eb2-82078e8949fa>
2013-03-23 09:27:39 +0000

    主页设计10%

--- trunk/TeamHome/WebRoot/js/admin_menu.js
+++ trunk/TeamHome/WebRoot/js/admin_menu.js
@@ -3,4 +3,12 @@ $(function(){
         e.preventDefault();
         $(this).tab('show');
     });
+    //todo 正在测试json获取的数据
+    $.ajax({
+        url:'menu/load.action',
+        type:'post',
+        success:function(data){
+            console.log(data);
+        }
+    });
 });
