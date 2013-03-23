eeb31a3c252e1894961030ecaf8b0bf4f9ef6607
xjyaitf@gmail.com <xjyaitf@gmail.com@ee9c0801-8353-6db2-6eb2-82078e8949fa>
2013-03-23 16:57:56 +0000

    菜单界面完善

--- TeamHome/WebRoot/js/admin_menu.js
+++ TeamHome/WebRoot/js/admin_menu.js
@@ -1,14 +1,55 @@
-$(function(){
+$(function () {
     $("#menu-tab a").click(function (e) {
         e.preventDefault();
         $(this).tab('show');
     });
-    //todo 正在测试json获取的数据
     $.ajax({
+        //获取菜单数据
         url:'menu/load.action',
         type:'post',
-        success:function(data){
-            console.log(data);
+        success:function (data) {
+            var menus = data.list;
+            var html = [];
+            //拼html语句来显示menu
+            for (var i in menus) {
+                var menu = menus[i];
+                html.push("<tr>");
+                html.push("<td>" + menu.id + "</td>");
+                html.push("<td>" + menu.code + "</td>");
+                html.push("<td>" + menu.name + "</td>");
+                html.push("<td>" + menu.link + "</td>");
+                html.push("<td>" +
+                    "<a href=\"javascript:update(" + menu.id + ")\" class=\"btn btn-small btn-warning\">修改</a>" +
+                    "<a href=\"#\" class=\"btn btn-small btn-danger\">删除</a>" +
+                    "</td>");
+                html.push("</tr>");
+            }
+            $("#look_menu tbody").html(html);
         }
     });
 });
+function update(id) {
+    $.getJSON('menu/get.action', {id:id}, function (data) {
+        $('#menu_dialog_body').empty();
+        var menu = data.model;
+        var form = '<form class="form-horizontal" action="menu/update.action" method="post"></form>';
+        var html = "";
+        for (var i in menu) {
+            if (i != 'id') {
+                html+="<div class=\"control-group\">";
+                html+="    <label class=\"control-label\" for=\"" + i + "\">" + i + "</label>";
+                html+="    <div class=\"controls\">";
+                html+="      <input type=\"text\" id=\"" + i + "\" value='" + menu[i] + "'>";
+                html+="    </div>";
+                html+="  </div>";
+            }
+        }
+        var button = '<div class="control-group">' +
+            '<div class="controls">' +
+            '<input type="submit" class="btn btn-primary" value="保存修改">' +
+            '</div>' +
+            '</div>';
+        $(form).append(html).append(button).appendTo('#menu_dialog_body');
+        $('#menu_dialog').modal('show');
+    });
+}
--- TeamHome/src/com/teamhome/action/MenuAction.java
+++ TeamHome/src/com/teamhome/action/MenuAction.java
@@ -6,66 +6,84 @@ import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 
+import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import com.teamhome.dao.MenuDao;
 import com.teamhome.dto.Menu;
 
-@ParentPackage("json-default")
+@ParentPackage("json-interceptor")
 @Namespace("/menu")
-public class MenuAction implements ModelDriven<Menu> {
+@Results({ @Result(name = "success",type = "json",params = {"root","action"}) })
+public class MenuAction extends ActionSupport implements ModelDriven<Menu> {
 
-	Menu menu = new Menu();//表单传值的接收对象
-	MenuDao dao = new MenuDao();//dao对象
-	boolean success = false;//返回的json
-	ArrayList<Menu> list = new ArrayList<Menu>();//返回的json
+	private static final long serialVersionUID = 8105088880041737773L;
 
-	//获取所有菜单列表
-	@Action(value = "load", results = { @Result(name = "success", type = "json") })
+	private Menu menu = new Menu();// 表单传值的接收对象
+	private MenuDao dao = new MenuDao();// dao对象
+	private boolean success;// 返回的json
+	private ArrayList<Menu> list;// 返回的json
+
+	// 获取所有菜单列表
+	@Action(value = "load")
 	public String load() {
 		list = dao.load();
+		if(list.size() > 0){
+			success = true;
+		} else {
+			success = false;
+		}
 		return "success";
 	}
-	
-	//增加一个菜单
-	@Action(value = "add", results = { @Result(name = "success", type = "json") })
-	public String add(){
-		dao.add(menu);
+
+	// 增加一个菜单
+	@Action(value = "add")
+	public String add() {
+		int i = dao.add(menu);
+		if(i > 0) {
+			success = true;
+		} else {
+			success = false;
+		}
 		return "success";
 	}
-	
-	//删除一个菜单
-	@Action(value = "delete", results = { @Result(name = "success", type = "json") })
-	public String delete(){
+
+	// 删除一个菜单
+	@Action(value = "delete")
+	public String delete() {
 		success = dao.delete(menu.getId());
 		return "success";
 	}
-	
-	//更新一个菜单
-	@Action(value = "update", results = { @Result(name = "success", type = "json") })
-	public String update(){
+
+	// 更新一个菜单
+	@Action(value = "update")
+	public String update() {
 		success = dao.update(menu.getId(), menu);
 		return "success";
 	}
 	
+	//获取一个菜单的属性
+	@Action(value = "get ")
+	public String get(){
+		menu = dao.get(menu.getId());
+		return SUCCESS;
+	}
+
 	public ArrayList<Menu> getList() {
 		return list;
 	}
 
-	public void setList(ArrayList<Menu> list) {
-		this.list = list;
-	}
-	
 	public boolean isSuccess() {
 		return success;
 	}
-
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
-
+	
 	public Menu getModel() {
 		return menu;
+	}
+
+	public void setMenu(Menu menu) {
+		this.menu = menu;
 	}
 
 }
